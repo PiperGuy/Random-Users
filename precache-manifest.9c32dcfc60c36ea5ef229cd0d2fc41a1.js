3a87ab3fa015950162d4d3e7b6a5cec03a7a3c7b
naveenpandurangi <naveenpandurangi@yahoo.com>
2020-01-27 15:28:39 +0530

    Updates

---
+++ precache-manifest.9c32dcfc60c36ea5ef229cd0d2fc41a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a401e90333d27f146791e590a8488085",
+    "url": "/Random-Users/index.html"
+  },
+  {
+    "revision": "955672bddffe826543c6",
+    "url": "/Random-Users/static/css/main.236f4322.chunk.css"
+  },
+  {
+    "revision": "c8b6253b7e7aac04cdf5",
+    "url": "/Random-Users/static/js/2.66b0c319.chunk.js"
+  },
+  {
+    "revision": "955672bddffe826543c6",
+    "url": "/Random-Users/static/js/main.ba6c222e.chunk.js"
+  },
+  {
+    "revision": "fb083905ad00052244e1",
+    "url": "/Random-Users/static/js/runtime-main.9642621a.js"
+  },
+  {
+    "revision": "a5e99ce770a9f818352c5b00e1be0790",
+    "url": "/Random-Users/static/media/icon_locationPin.a5e99ce7.png"
+  },
+  {
+    "revision": "488071d3f6873cce41af8da342b675d2",
+    "url": "/Random-Users/static/media/icon_search.488071d3.png"
+  }
+]);
